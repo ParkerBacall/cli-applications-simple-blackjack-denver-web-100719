7f787049925c64461dc8a897f6d4b1cc18358d25
Parker Bacall <pbacall2@gmail.com>
2019-11-12 02:26:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,8 +63,8 @@ def runner()
   while card_total < 21 do
     welcome
     card_total = initial_round
-    hit?(card_total)
+    = card_total = hit?(card_total)
     display_card_total(card_total)
   end
-end_game(card_total)
+  end_game(card_total)
 end
